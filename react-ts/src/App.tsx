b51ac91baa7ba4648eb9a40818e88c7532d6b831
Dionata Lima <67246904+DionataLima@users.noreply.github.com>
2024-10-02 23:01:09 -0300

    Update App.tsx

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
           }
         />
   
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
       </Routes>
     </Router>
   );
